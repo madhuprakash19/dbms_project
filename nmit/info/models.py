c383c9e0d1ce6b952ba57d7fbb9faaeeeb802041
madhuprakash19 <madhutp11@gmail.com>
2021-12-05 09:53:40 +0530

    added predefined fields

--- nmit/info/models.py
+++ nmit/info/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class hostel(models.Model):
+    id = models.IntegerField(primary_key=True)
+    name = models.CharField(max_length=10)
+
+    def __str__(self):
+        return str(self.id)+self.name
+
+
+class department(models.Model):
+    id = models.IntegerField(primary_key=True)
+    name = models.CharField(max_length=20)
+    code = models.CharField(max_length=10)
+    HOD = models.ForeignKey(User,related_name='HOD',on_delete = models.CASCADE,blank=True)
+
+    def __str__(self):
+        return str(self.id)+self.name
+
+class designation(models.Model):
+    id = models.IntegerField(primary_key=True)
+    name = models.CharField(max_length=10)
+
+    def __str__(self):
+        return str(self.id)+self.name
--- nmit/user/models.py
+++ nmit/user/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+class user_status(models.Model):
+    id = models.IntegerField(primary_key=True)
+    name = models.CharField(max_length=10)
+
+    def __str__(self):
+        return str(self.id)+self.name
+
+
+class role(models.Model):
+    id = models.IntegerField(primary_key=True)
+    name = models.CharField(max_length=10)
+
+    def __str__(self):
+        return str(self.id)+self.name
