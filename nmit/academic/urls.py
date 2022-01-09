432d4360afb1618ede09d70f4071ab0305ebbd00
madhuprakash19 <madhutp11@gmail.com>
2022-01-09 18:46:16 +0530

    added attedence count feature

--- nmit/academic/urls.py
+++ nmit/academic/urls.py
@@ -5,6 +5,7 @@ app_name = 'academic'
 urlpatterns = [
     path('subject/',views.subject,name='teacher_subject'),
     path('view_class/<int:id>/',views.view_class,name='view_class'),
-    path('markattendence/<int:class_id>/<int:attendence_id>',views.mark_attendence,name='mark_attendence'),
-    path('attendence_schedule/<int:class_id>/<int:subject_id>',views.attendence_schedule,name='attendence_schedule'),
+    path('markattendence/<int:class_id>/<int:attendence_id>/<int:subject_id>',views.mark_attendence,name='mark_attendence'),
+    path('attendence_schedule/<int:class_id>/<int:subject_id>',views.attendence_schedules,name='attendence_schedule'),
+    path('attedence/save_attendence/',views.save_attendence,name='save_attendence'),
 ]
--- nmit/academic/views.py
+++ nmit/academic/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render,get_object_or_404,get_list_or_404
 from django.contrib.auth.decorators import login_required
-from academic.models import faculty_handled_class,sub_class,registered_students,main_class,class_subject
+from academic.models import attendence_count,faculty_handled_class,sub_class,registered_students,main_class,class_subject,attendence_schedule,attendence
 from .forms import AttendenceForm
 from django.http import HttpResponseRedirect
 from django.urls import reverse
+from django.contrib.auth.models import User
 # Create your views here.
 @login_required
 def subject(request):
@@ -17,13 +18,24 @@ def view_class(request,id):
     mclass = main_class.objects.get(id=sclass.parent_class.id)
     subjects = list(class_subject.objects.filter(class_id=mclass))
     student_list = list(registered_students.objects.filter(class_id=sclass).order_by('student'))
-    return render(request,'view_class.html',{'student_list':student_list,'sclass':sclass,'subjects':subjects,'fsubjects':fsubjects})
 
-def mark_attendence(request,class_id,attendence_id):
+    a={}
+    for i in subjects:
+        a[i.class_subject]={'class_id':0,'subject_id':0}
+    for i in fsubjects:
+        a[i.subject.class_subject]['class_id']=sclass.id
+        a[i.subject.class_subject]['subject_id']=i.id
+    print(a)
+
+
+    return render(request,'view_class.html',{'student_list':student_list,'sclass':sclass,'a':a})
+
+def mark_attendence(request,class_id,attendence_id,subject_id):
     sclass = sub_class.objects.get(id=class_id)
-    return render(request,'mark_attendence.html',{'sclass':sclass})
+    return render(request,'mark_attendence.html',{'sclass':sclass,'attendence_id':attendence_id,'subject_id':subject_id,'class_id':class_id})
 
-def attendence_schedule(request,class_id,subject_id):
+def attendence_schedules(request,class_id,subject_id):
+    attendence_list = list(attendence_schedule.objects.filter(subject__id=subject_id).order_by('-id')) #double __ is used to point the id of the pointing foreign key object
     if request.method == 'POST':
         attendence_form = AttendenceForm(data = request.POST)
 
@@ -33,11 +45,125 @@ def attendence_schedule(request,class_id,subject_id):
             a.subject = faculty_handled_class.objects.get(id=subject_id)
             a.save()
             attendence_id = a.id
-            print(attendence_id)
-            print(class_id)
-            return HttpResponseRedirect(reverse('academic:mark_attendence',args=[class_id,attendence_id]))
+            return HttpResponseRedirect(reverse('academic:mark_attendence',args=[class_id,attendence_id,subject_id]))
 
     else:
         attendence_form = AttendenceForm()
 
-    return render(request,'attendence_schedule.html',{'attendence_form':attendence_form})
+    return render(request,'attendence_schedule.html',{'attendence_form':attendence_form,'attendence_list':attendence_list})
+
+
+def save_attendence(request):
+    attendence_list = attendence_schedule.objects.get(id=request.POST['attendence_id'])
+    class_id = request.POST['class_id']
+    subject_id = request.POST['subject_id']
+    subject = faculty_handled_class.objects.get(id=subject_id)
+    sclass = sub_class.objects.get(id=class_id)
+    b=[]
+    for i in sclass.students.all():
+        b.append(i.username)
+    for i,j in request.POST.items():
+        if i in b:
+            status = j
+            if status == 'present':
+                status = True
+            else:
+                status = False
+            stud = get_object_or_404(User,username = i)
+            if attendence_list.status == True:
+                try:
+                    a = attendence.objects.get(subject=attendence_list,student = stud )
+                    count_edit(a,status,subject,stud,sclass)
+                    a.status = status
+                    a.save()
+                except attendence.DoesNotExist:
+                    a = attendence(subject=attendence_list,student = stud,status = status)
+                    a.save()
+                    count_add(status,subject,stud,sclass)
+            else:
+                a = attendence(subject=attendence_list,student = stud ,status = status)
+                a.save()
+                attendence_list.status = True
+                attendence_list.save()
+                count_add(status,subject,stud,sclass)
+    return HttpResponseRedirect(reverse('academic:attendence_schedule',args=[class_id,subject_id]))
+
+
+def count_edit(old,new,subject,student,sclass):
+    if old.status==False and new==True:
+        try:
+            acount = attendence_count.objects.get(subject=subject,student=student)
+            acount.attended_class += 1
+            acount.save()
+        except:
+            acount = attendence_count(subject=subject,student=student,sclass=sclass)
+            acount.save()
+            acount.total_class +=1
+            acount.attended_class += 1
+            acount.save()
+    elif old.status==True and new==False:
+        try:
+            acount = attendence_count.objects.get(subject=subject,student=student)
+            acount.attended_class -= 1
+            acount.save()
+        except:
+            acount = attendence_count(subject=subject,student=student,sclass=sclass)
+            acount.save()
+            acount.total_class +=1
+            acount.save()
+
+def count_add(new,subject,student,sclass):
+    if new:
+        try:
+            acount = attendence_count.objects.get(subject=subject,student=student)
+            acount.attended_class += 1
+            acount.total_class +=1
+            acount.save()
+        except:
+            acount = attendence_count(subject=subject,student=student,sclass=sclass)
+            acount.save()
+            acount.total_class +=1
+            acount.attended_class += 1
+            acount.save()
+    else:
+        try:
+            acount = attendence_count.objects.get(subject=subject,student=student)
+            acount.total_class +=1
+            acount.save()
+        except:
+            acount = attendence_count(subject=subject,student=student,sclass=sclass)
+            acount.save()
+            acount.total_class +=1
+            acount.save()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#
